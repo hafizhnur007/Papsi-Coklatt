a26936b8b087048baff51d75ed4a8377d3cf6b86
dilaaxx <106157612+dilaaxx@users.noreply.github.com>
2022-06-08 01:46:30 +0700

    Dila Ambar - Marketing
    pas di run ga munculll

---
+++ Papsi-Coklatt/Papsi-Coklatt/Marketing.Designer.vb
@@ -0,0 +1,325 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Marketing
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtcustomer = New System.Windows.Forms.TextBox()
+        Me.txtalamat = New System.Windows.Forms.TextBox()
+        Me.txtnotelp = New System.Windows.Forms.TextBox()
+        Me.txtjenisproduk = New System.Windows.Forms.TextBox()
+        Me.txtjumlahproduk = New System.Windows.Forms.TextBox()
+        Me.txttanggalorder = New System.Windows.Forms.DateTimePicker()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.DGV = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(65, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Customer"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(65, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Alamat"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(65, 123)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "No. Telp"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(65, 162)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(110, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Tanggal Order"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(65, 206)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Jenis Produk"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(65, 245)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(114, 20)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Jumlah Produk"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(69, 291)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(74, 20)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Deskripsi"
+        '
+        'txtcustomer
+        '
+        Me.txtcustomer.Location = New System.Drawing.Point(197, 33)
+        Me.txtcustomer.Name = "txtcustomer"
+        Me.txtcustomer.Size = New System.Drawing.Size(210, 26)
+        Me.txtcustomer.TabIndex = 7
+        '
+        'txtalamat
+        '
+        Me.txtalamat.Location = New System.Drawing.Point(197, 77)
+        Me.txtalamat.Name = "txtalamat"
+        Me.txtalamat.Size = New System.Drawing.Size(210, 26)
+        Me.txtalamat.TabIndex = 8
+        '
+        'txtnotelp
+        '
+        Me.txtnotelp.Location = New System.Drawing.Point(197, 123)
+        Me.txtnotelp.Name = "txtnotelp"
+        Me.txtnotelp.Size = New System.Drawing.Size(210, 26)
+        Me.txtnotelp.TabIndex = 9
+        '
+        'txtjenisproduk
+        '
+        Me.txtjenisproduk.Location = New System.Drawing.Point(197, 206)
+        Me.txtjenisproduk.Name = "txtjenisproduk"
+        Me.txtjenisproduk.Size = New System.Drawing.Size(210, 26)
+        Me.txtjenisproduk.TabIndex = 10
+        '
+        'txtjumlahproduk
+        '
+        Me.txtjumlahproduk.Location = New System.Drawing.Point(197, 245)
+        Me.txtjumlahproduk.Name = "txtjumlahproduk"
+        Me.txtjumlahproduk.Size = New System.Drawing.Size(210, 26)
+        Me.txtjumlahproduk.TabIndex = 11
+        '
+        'txttanggalorder
+        '
+        Me.txttanggalorder.Location = New System.Drawing.Point(197, 162)
+        Me.txttanggalorder.Name = "txttanggalorder"
+        Me.txttanggalorder.Size = New System.Drawing.Size(210, 26)
+        Me.txttanggalorder.TabIndex = 12
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(68, 344)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 32)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Input"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(292, 392)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 30)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(292, 344)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 32)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "Edit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(180, 392)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 30)
+        Me.Button4.TabIndex = 16
+        Me.Button4.Text = "Delete"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(180, 344)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 32)
+        Me.Button5.TabIndex = 17
+        Me.Button5.Text = "Save"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(69, 392)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 30)
+        Me.Button6.TabIndex = 18
+        Me.Button6.Text = "Cancel"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'DGV
+        '
+        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6})
+        Me.DGV.Location = New System.Drawing.Point(467, 32)
+        Me.DGV.Name = "DGV"
+        Me.DGV.RowHeadersWidth = 62
+        Me.DGV.RowTemplate.Height = 28
+        Me.DGV.Size = New System.Drawing.Size(718, 239)
+        Me.DGV.TabIndex = 19
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Customer"
+        Me.Column1.MinimumWidth = 8
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 150
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Alamat"
+        Me.Column2.MinimumWidth = 8
+        Me.Column2.Name = "Column2"
+        Me.Column2.Width = 150
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "No. Telp"
+        Me.Column3.MinimumWidth = 8
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 150
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Tanggal Order"
+        Me.Column4.MinimumWidth = 8
+        Me.Column4.Name = "Column4"
+        Me.Column4.Width = 150
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Jenis Produk"
+        Me.Column5.MinimumWidth = 8
+        Me.Column5.Name = "Column5"
+        Me.Column5.Width = 150
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Jumlah Produk"
+        Me.Column6.MinimumWidth = 8
+        Me.Column6.Name = "Column6"
+        Me.Column6.Width = 150
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1238, 520)
+        Me.Controls.Add(Me.DGV)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txttanggalorder)
+        Me.Controls.Add(Me.txtjumlahproduk)
+        Me.Controls.Add(Me.txtjenisproduk)
+        Me.Controls.Add(Me.txtnotelp)
+        Me.Controls.Add(Me.txtalamat)
+        Me.Controls.Add(Me.txtcustomer)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtcustomer As TextBox
+    Friend WithEvents txtalamat As TextBox
+    Friend WithEvents txtnotelp As TextBox
+    Friend WithEvents txtjenisproduk As TextBox
+    Friend WithEvents txtjumlahproduk As TextBox
+    Friend WithEvents txttanggalorder As DateTimePicker
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents DGV As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+End Class
---
+++ Papsi-Coklatt/Papsi-Coklatt/Marketing.vb
@@ -0,0 +1,136 @@
+﻿Public Class Marketing
+    Sub KosongkanForm()
+        txtcustomer.Text = ""
+        txtalamat.Text = ""
+        txtnotelp.Text = ""
+        txttanggalorder.Text = ""
+        txtjenisproduk.Text = ""
+        txtjumlahproduk.Text = ""
+        txtcustomer.Focus()
+        'Coding diatas ada untuk mengosongkan Form yang sudah kita buat ,
+
+    End Sub
+    Sub MatikanForm()
+        txtcustomer.Enabled = False
+        txtalamat.Enabled = False
+        txtnotelp.Enabled = False
+        txttanggalorder.Enabled = False
+        txtjenisproduk.Enabled = False
+        txtjumlahproduk.Enabled = False
+        'Coding diatas untuk menutup atau mematikan form agar tidak dapat isi
+    End Sub
+    Sub HidupkanForm()
+        txtcustomer.Enabled = True
+        txtalamat.Enabled = True
+        txtnotelp.Enabled = True
+        txttanggalorder.Enabled = True
+        txtjenisproduk.Enabled = True
+        txtjumlahproduk.Enabled = True
+        'Coding diatas untuk mulai menghidupkan form agar form bisa diisi
+    End Sub
+    Sub TampilkanData()
+        Call koneksiDB()
+        DA = New OleDb.OleDbDataAdapter("select * from Buku ", Conn)
+        DS = New DataSet
+        DA.Fill(DS)
+        DGV.DataSource = DS.Tables(0)
+        DGV.ReadOnly = True
+        'Coding diatas untuk menampilkan data dari Ms.Access yang sudah kita
+
+    End Sub
+    Private Sub Marketing_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call MatikanForm()
+        Call TampilkanData()
+
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Call HidupkanForm()
+        Call KosongkanForm()
+        ' Fungsi coding sama dengan diatas
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Call MatikanForm()
+        Call KosongkanForm()
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        If txtcustomer.Text = "" Or txtalamat.Text = "" Or txtnotelp.Text = "" Or txttanggalorder.Text = "" Or txtjenisproduk.Text = "" Or txtjumlahproduk.Text = "" Then
+            MsgBox("Data Order Belum Lengkap")
+            Exit Sub
+            'Coding ini berfungsi untuk mengecek jika salah satu field data
+        Else
+            Call koneksiDB()
+            CMD = New OleDb.OleDbCommand(" select * from Customer where Id_Customer='" & txtcustomer.Text & "'", Conn)
+            DM = CMD.ExecuteReader
+            DM.Read()
+            If Not DM.HasRows Then
+                Call koneksiDB()
+                Dim simpan As String
+                simpan = "insert into Produk values ('" & txtcustomer.Text & "', '" & txtalamat.Text & "', '" & txtnotelp.Text & "','" & txttanggalorder.Text & "','" & txtjenisproduk.Text & "','" & txtjumlahproduk.Text & "')"
+                CMD = New OleDb.OleDbCommand(simpan, Conn)
+                CMD.ExecuteNonQuery()
+                MsgBox("Input Data Sukses")
+            Else
+                MsgBox("Data Sudah Ada")
+            End If
+            Call MatikanForm()
+            Call KosongkanForm()
+            Call TampilkanData()
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If txtcustomer.Text = "" Or txtalamat.Text = "" Or txtnotelp.Text = "" Or txttanggalorder.Text = "" Or txtjenisproduk.Text = "" Or txtjumlahproduk.Text = "" Then
+            MsgBox("Data Order Belum Lengkap")
+            Exit Sub
+        Else
+            Call koneksiDB()
+            CMD = New OleDb.OleDbCommand(" update Order set Nama_Customer = '" & txtcustomer.Text & "', Alamat ='" & txtalamat.Text & "',No_Telp = '" & txtnotelp.Text & "',Tanggal_Order = '" & txttanggalorder.Text & "',Jenis_Produk= '" & txtjenisproduk.Text & "',Jumlah_Produk = '" & txtjumlahproduk.Text, Conn)
+            DM = CMD.ExecuteReader
+            MsgBox("Update Data Berhasil")
+        End If
+        Call KosongkanForm()
+        Call MatikanForm()
+        Call TampilkanData()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If txtcustomer.Text = "" Then
+            MsgBox("Tidak ada data order yang dipilih")
+            Exit Sub
+        Else
+            If MessageBox.Show("Are you sure to delete this data ? ",
+"Konfirmasi", MessageBoxButtons.YesNoCancel) = Windows.Forms.DialogResult.Yes Then
+                Call koneksiDB()
+                CMD = New OleDb.OleDbCommand(" delete from Customer where
+Nama_Customer ='" & txtcustomer.Text & "'", Conn)
+                DM = CMD.ExecuteReader
+                MsgBox("Hapus Data Berhasil")
+                Call MatikanForm()
+                Call KosongkanForm()
+                Call TampilkanData()
+            Else
+                Call KosongkanForm()
+                Call TampilkanData()
+            End If
+        End If
+    End Sub
+
+    Private Sub DGV_CellContentClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellMouseEventArgs) Handles DGV.CellMouseClick
+        On Error Resume Next
+        txtcustomer.Text = DGV.Rows(e.RowIndex).Cells(0).Value
+        txtalamat.Text = DGV.Rows(e.RowIndex).Cells(1).Value
+        txtnotelp.Text = DGV.Rows(e.RowIndex).Cells(2).Value
+        txttanggalorder.Text = DGV.Rows(e.RowIndex).Cells(3).Value
+        txtjenisproduk.Text = DGV.Rows(e.RowIndex).Cells(4).Value
+        txtjumlahproduk.Text = DGV.Rows(e.RowIndex).Cells(5).Value
+        Call HidupkanForm()
+        txtcustomer.Enabled = False 'Ketika mengedit , kita tidak bisa
+
+    End Sub
+End Class
