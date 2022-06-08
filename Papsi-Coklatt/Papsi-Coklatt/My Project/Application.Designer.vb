408449b290cedfb959a6aeeb668428c0fb62d257
hafizhnur007 <hafizhnurrasyadan@gmail.com>
2022-06-08 14:05:12 +0700

    Nih v2.0

--- Papsi-Coklatt/Papsi-Coklatt/My Project/Application.Designer.vb
+++ Papsi-Coklatt/Papsi-Coklatt/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Papsi_Coklatt.Marketing
+            Me.MainForm = Global.Papsi_Coklatt.Warehouse
         End Sub
     End Class
 End Namespace
